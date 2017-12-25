83bf14211bf0130ee300f26376749b094db46e35
alexeyradkovich <alexey.radkovich@gmail.com>
2017-12-25 18:12:53 +0300

    displaying brand names in the table

--- server/repositories/getBrandsByFilter.pipeline.js
+++ server/repositories/getBrandsByFilter.pipeline.js
@@ -1,27 +1,29 @@
 module.exports = function(platform, month, year) {
-    return [
-        {
-            "$project" : {
-                "_id": 0,
-                "name" : 1,
-                "pl": { $in: [ platform, "$platforms" ] },
-                "month": { $eq: [ {$month: "$dates.modified"}, month ] },
-                "year": { $eq: [ {$year: "$dates.modified"}, year ] }
-            }
-        },
-        {
-            "$match" : {
-                "pl" : true,
-                "month" : true,
-                "year" : true,
-                "name": {$exists: true}
-            }
-        },
-        {
-            "$project" : {
-                "_id": 0,
-                "name" : 1
-            }
-        }
-    ]
+	return [
+		{
+			"$project" : {
+				"_id": 0,
+				"name" : {$cond: { if: { $eq: [ "$brand_name", "" ] }, 
+									then: "$name", 
+									else: "$brand_name" } },
+				"pl": { $in: [ platform, "$platforms" ] },
+				"month": { $eq: [ {$month: "$dates.modified"}, month ] },
+				"year": { $eq: [ {$year: "$dates.modified"}, year ] }
+			}
+		},
+		{
+			"$match" : {
+				"pl" : true,
+				"month" : true,
+				"year" : true,
+				"name": {$exists: true}
+			}
+		},
+		{
+			"$project" : {
+				"_id": 0,
+				"name" : 1
+			}
+		}
+	]
 }
