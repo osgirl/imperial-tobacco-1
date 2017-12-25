9fa1caa38474db74f9f6959e38e5ad3adfe888ab
alexeyradkovich <alexey.radkovich@gmail.com>
2017-12-25 13:04:15 +0300

    doesnt display already added tags in autocomlete; ui fixes

--- client/src/app/pipes/unique.pipe.ts
+++ client/src/app/pipes/unique.pipe.ts
@@ -1,18 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-	name: 'unique'
+	name: 'unique',
+	pure: false
 })
 
 export class Unique implements PipeTransform {
-	transform(filteredArray: Array<any>): any {
+	transform(filteredArray: Array<any>, alreadyAddedBrands: any[]): any {
 		if(!filteredArray) return filteredArray;
 
 		let brands = filteredArray;
 		let uniqueBrands: any[] = [];
 
 		brands.forEach((brand) => {
-			if(!uniqueBrands.find(x => x.name === brand.name)) uniqueBrands.push(brand);
+			if(!uniqueBrands.find(x => x.name === brand.name) && !alreadyAddedBrands.find(x => x.name === brand.name)) uniqueBrands.push(brand);
 		})
 
 		return uniqueBrands;
